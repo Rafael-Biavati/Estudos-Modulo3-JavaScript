b87079379d9e02f83dce1ce6fe37100cd8af3da1
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-06-01 17:23:25 -0300

    Create script.js

---
+++ ex001/script.js
@@ -0,0 +1,14 @@
+if(true){
+    var exposta = "exposta"
+    let naoExposta = "nao exposta"
+}
+
+for(let i = 0;i<10;i++){
+    var j = i;
+}
+
+console.log(exposta);
+//console.log(naoExposta);
+
+console.log(j);
+//console.log(i);
