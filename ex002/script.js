4bf6224ab5f9b2dbc1aee6749071bb2a58a51871
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-06-02 15:36:34 -0300

    Create script.js

---
+++ ex002/script.js
@@ -0,0 +1,13 @@
+//arrow functions
+
+/*const minhaF = function(){
+    console.log("teste");
+}
+
+function minhaF() {
+    console.log("teste");
+}*/
+
+const funcao = () => "teste arrow function";
+console.log(funcao());
+
