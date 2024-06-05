3f1b9b3a29a073c441d1e5cb07388b7381325660
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-06-05 17:23:34 -0300

    ex003

--- ex003/js/script.js
+++ ex003/js/script.js
@@ -1,2 +1,14 @@
-//IIFE
+//IIFE - Immediately Invoked Function Expression
 
+const resultado = (function() {
+    const numeros = [1, 2, 3, 4, 5];
+    let soma = 0;
+    
+    for (let i = 0; i < numeros.length; i++) {
+        soma += numeros[i];
+    }
+    
+    return soma;
+})();
+
+console.log(resultado);
